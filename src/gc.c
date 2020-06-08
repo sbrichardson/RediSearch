95d4ae365ce202b725ccce55cc67b3e5545715ce
ariel shtul <ashtul@gmail.com>
2020-06-08 14:02:11 +0300

    return lock of GIL

--- src/gc.c
+++ src/gc.c
@@ -201,7 +201,9 @@ void GC_ThreadPoolStart() {
 
 void GC_ThreadPoolDestroy() {
   if (gcThreadpool_g != NULL) {
+    RedisModule_ThreadSafeContextUnlock(RSDummyContext);
     thpool_destroy(gcThreadpool_g);
     gcThreadpool_g = NULL;
+    RedisModule_ThreadSafeContextLock(RSDummyContext);
   }
 }
